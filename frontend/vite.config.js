5acbb1a74a4edec63ce312cd946f551524af859b
Medipelly Manvith Kumar <m6783321@gmail.com>
2025-08-20 22:06:19 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://volmant.onrender.com/',
         changeOrigin: true,
         secure: false,
       }
