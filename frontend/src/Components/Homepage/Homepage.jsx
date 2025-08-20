b10ba402784224ee90ad1f1693a524d6bbdb962d
Medipelly Manvith Kumar <m6783321@gmail.com>
2025-08-20 22:55:20 +0530

    first commit

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -12,7 +12,7 @@ const Homepage = () => {
   };
   const [data,setdata]=useState();
   useEffect(()=>{
-    axios.get("/api/promoted/view")
+    axios.get("https://volmant.onrender.com/api/promoted/view")
     .then(res=>{setdata(res.data)
     })
     .catch(error=>console.log(error))
